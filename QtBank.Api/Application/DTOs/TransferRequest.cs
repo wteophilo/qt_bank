1c0eabc6c847eb96e31b63ebca7324b84a000975
Willian Teophilo <wteophilo@gmail.com>
2026-06-26 22:41:58 -0300

    docs(api): add Swagger request examples via XML documentation comments
        - Add example attributes for Username parameter in TokenRequest schema
        - Add example attributes for TransferRequest, DepositRequest, and WithdrawalRequest schemas
        - Map example values to match seeded mock data accounts (111111, 222222)

--- QtBank.Api/Application/DTOs/TransferRequest.cs
+++ QtBank.Api/Application/DTOs/TransferRequest.cs
@@ -7,6 +7,11 @@ namespace QtBank.Api.Application.DTOs;
 /// <summary>
 /// HTTP request body schema for transferring money between two accounts.
 /// </summary>
+/// <param name="SourceAccountNumber" example="222222">The source account number to transfer funds from.</param>
+/// <param name="DestinationAccountNumber" example="111111">The destination account number to transfer funds to.</param>
+/// <param name="Amount" example="49.50">The amount of money to transfer.</param>
+/// <param name="Currency" example="USD">The currency of the transaction (USD, EUR, BRL).</param>
+/// <param name="IdempotencyKey" example="b32454da-8785-48fa-86e0-3fb364673891">A unique GUID to identify the request and prevent duplicate transfers.</param>
 [method: JsonConstructor]
 public record TransferRequest(
     string SourceAccountNumber,
